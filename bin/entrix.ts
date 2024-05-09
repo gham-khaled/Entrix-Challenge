88bd4378e36b8c82fd212b8efc34bd0817b80ea5
Khaled Ghamgui <gham.khaled@gmail.com>
2024-05-09 12:58:41 +0100

    Add Empty CodePipeline

--- bin/entrix.ts
+++ bin/entrix.ts
@@ -3,12 +3,14 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import {EntrixOrdersStack} from '../lib/entrixOrders';
 import {EntrixDataPipelineStack} from "../lib/entrixDataPipeline";
+import {EntrixCICDPipeline} from "../lib/entrixCICDPipeline";
 
 const app = new cdk.App();
 const envDev = {account: '830962405258', region: 'eu-west-1'}
-new EntrixOrdersStack(app, 'EntrixOrders', {
-    env: envDev
-});
-new EntrixDataPipelineStack(app, 'EntrixDataPipeline', {
-    env: envDev
-});
+// new EntrixOrdersStack(app, 'EntrixOrders', {
+//     env: envDev
+// });
+// new EntrixDataPipelineStack(app, 'EntrixDataPipeline', {
+//     env: envDev
+// });
+new EntrixCICDPipeline(app, 'EntrixCICDPipeline', {env: envDev})
